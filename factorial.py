f07ad541e95a40d22d83c478ad872efcc834b0c5
kazzastic <rkazim79@gmail.com>
2018-12-18 17:15:30 +0500

    factorial.py

---
+++ factorial.py
@@ -0,0 +1,13 @@
+def recursion_fact(num):
+    if num == 1:
+       return num
+    else:
+       return num*recursion_fact(num-1)
+num = int(input("The number please: "))
+
+if num < 0:
+    print("Sorry no negative numbers!")
+elif num == 0:
+    print("this number is 0 !")
+else:
+    print("The factorial of number is: ", recursion_fact(num))
