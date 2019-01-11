b0d703fe5fa9eaccbba8ca93a37ab31b9e55f5e4
kazzastic <rkazim79@gmail.com>
2019-01-11 16:56:04 +0500

    testing

--- testing.py
+++ testing.py
@@ -3,101 +3,51 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-#The sensor in the front
-GPIO_trig1 = 19
-GPIO_echo1 = 26
+#MOTOR NUMBER 1
+in1 = 12
+in2 = 16
+en = 20
+temp1 = 1
 
-GPIO.setup(GPIO_trig1, GPIO.OUT)
-GPIO.setup(GPIO_echo1, GPIO.IN)
-GPIO.setwarnings(False)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
 
-#The sensor 
-GPIO_trig2 = 27
-GPIO_echo2 = 22
+value=25
+'''
+PWN means the speed of the rotation of the wheels/motors of your robot 
+'''
+p = GPIO.PWM(en, 1000)
+p.start(value)
 
-GPIO.setup(GPIO_trig2, GPIO.OUT)
-GPIO.setup(GPIO_echo2, GPIO.IN)
+#MOTOR NUMBER 2
+in3 = 24
+in4 = 23
+em = 25
 
-#The sensor 
-GPIO_trig3 = 5
-GPIO_echo3 = 6
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+GPIO.setup(em, GPIO.OUT)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
 
-GPIO.setup(GPIO_trig3, GPIO.OUT)
-GPIO.setup(GPIO_echo3, GPIO.IN)
+'''
+Sets the PWM meaning how fast the tires/ motors are supposed to move 
+'''
+q = GPIO.PWM(em, 1000)
+q.start(value)
 
-def distance():
-    GPIO.output(GPIO_trig1, True)
 
-    time.sleep(0.00001)
-    GPIO.output(GPIO_trig1, False)
-
-    StartTime = time.time()
-    StopTime = time.time()
-
-    while GPIO.input(GPIO_echo1)==0:
-        StartTime = time.time()
-
-    while GPIO.input(GPIO_echo1)==1:
-        StopTime = time.time()
-
-    TimeE = StopTime - StartTime
-
-    dist = (TimeE*34300)/2
-
-    return dist
-
-def distance2():
-    GPIO.output(GPIO_trig2, True)
-
-    time.sleep(0.00001)
-    GPIO.output(GPIO_trig2, False)
-
-    StartTime = time.time()
-    StopTime = time.time()
-
-    while GPIO.input(GPIO_echo2)==0:
-        StartTime = time.time()
-
-    while GPIO.input(GPIO_echo2)==1:
-        StopTime = time.time()
-
-    TimeE = StopTime - StartTime
-
-    dist = (TimeE*34300)/2
-
-    return dist
-
-def distance3():
-    GPIO.output(GPIO_trig3, True)
-
-    time.sleep(0.00001)
-    GPIO.output(GPIO_trig3, False)
-
-    StartTime = time.time()
-    StopTime = time.time()
-
-    while GPIO.input(GPIO_echo3)==0:
-        StartTime = time.time()
-
-    while GPIO.input(GPIO_echo3)==1:
-        StopTime = time.time()
-
-    TimeE = StopTime - StartTime
-
-    dist = (TimeE*34300)/2
-
-    return dist
 if __name__ == '__main__':
     try:
         while True:
-            front = distance()
-            left = distance2()
-            right = distance3()
-            print("Measured Distance1 = "+str(front)+" cm")
-            print("Measured Distance2 = "+str(left)+" cm")
-            print("Measured Distance3 = "+str(right)+" cm")
-            #time.sleep(1)
-
+            GPIO.output(in1, False) #counter_clockwise
+            GPIO.output(in2, True)
+            GPIO.output(in3, True)
+            GPIO.output(in4, False) #counter_clockwise
+            print("Straight")
     except KeyboardInterrupt:
         print("Measurement stopped by user")
         GPIO.cleanup()
